42a48cd9e135dec5407f7a8f59994db24e08ad4a
IzzyAhnaf <izzyahnaf695@gmail.com>
2025-05-13 17:37:12 +0700

    commit

--- SPEDAL/backend/server.js
+++ SPEDAL/backend/server.js
@@ -24,7 +24,7 @@ fastify.register(cors, (instance) => {
 });
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'mysql',
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'spedal'
